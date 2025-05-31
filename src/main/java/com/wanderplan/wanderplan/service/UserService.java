89b342888127da42f86a5e6253ad5d2c231676b9
42varo1mst <42varo1mst@hft-stuttgart.de>
2025-05-31 20:55:21 +0200

    Users are now working

--- src/main/java/com/wanderplan/wanderplan/service/UserService.java
+++ src/main/java/com/wanderplan/wanderplan/service/UserService.java
@@ -1,40 +1,3 @@
-//package com.wanderplan.wanderplan.service;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//import com.wanderplan.wanderplan.model.User;
-//import com.wanderplan.wanderplan.repository.UserRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class UserService {
-//
-//    @Autowired
-//    private UserRepository UserRepository;
-//
-//    public User saveUser(User user) {
-//        return UserRepository.save(user);
-//    }
-//
-//    public Optional<User> getUserById(Long id) {
-//        return UserRepository.findById(id);
-//    }
-//
-//    public List<User> getAllUsers() {
-//        return UserRepository.findAll();
-//    }
-//
-//    public void deleteUser(Long id) {
-//        UserRepository.deleteById(id);
-//    }
-//
-//    public User getUserByEmail(String email) {
-//        return UserRepository.findByEmail(email);
-//    }
-//}
-
 package com.wanderplan.wanderplan.service;
 
 import java.util.List;
@@ -79,7 +42,12 @@ public class UserService {
         return userRepository.findById(id).map(user -> {
             user.setName(updatedUser.getName());
             user.setEmail(updatedUser.getEmail());
-            user.setPassword(updatedUser.getPassword());
+
+            // Only update the password if a new one is provided
+            if (updatedUser.getPassword() != null && !updatedUser.getPassword().isBlank()) {
+                user.setPassword(updatedUser.getPassword());
+            }
+
             return userRepository.save(user);
         }).orElseThrow(() -> new IllegalArgumentException("User not found with id: " + id));
     }
